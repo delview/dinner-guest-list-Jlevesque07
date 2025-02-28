923128938a4f40ab1761fc15fe967c9fb7ba04b2
Jlevesque07 <joshual251@deltalearns.ca>
2025-02-28 22:37:07 +0000

    Initial Commenting

---
+++ assignment_dinner_party.py
@@ -0,0 +1,30 @@
+# Dinner Party Assignment
+
+
+
+#Function to add a person to the guest list until variable for number of people who are going to be invited is zero
+def add(num_guests: int) -> list:
+    """Adds a person to the guest list until variable for number of people who are going to be invited is zero. """
+
+# Function to remove a person from the guest list and replace them with a new guest.
+def rar(guest: str) -> list:
+    """Removes a person from the guest list and replaces them with a new guest. """
+
+
+
+while True:
+    option = input("Would you like to: Add people to the list? [1], Replace someone in the list? [2], Print out a list of the invitations that will be sent out? [3], Exit the program? [4]")
+    
+    if int(option) == 1:
+        # Function "add" called. 
+
+    elif int(option) == 2:
+        # Function remove_and_replace (rar) called. 
+
+    elif int(option) == 3:
+        # Print out invitations for each dinner guest. 
+
+    elif int(option == 4):
+        # Exit the program.
+
+    
