af401c78c0a45b28bd375ba9b381c33e59f074d6
Jlevesque07 <joshual251@deltalearns.ca>
2025-03-02 04:05:57 +0000

    Added Code for the "remove and replace" function, added code for the prequisite to that function, and added a value error message for the main menu.

--- assignment_dinner_party.py
+++ assignment_dinner_party.py
@@ -34,22 +34,49 @@ def add() -> list:
 # Function to remove a person from the guest list and replace them with a new guest.
 def rar(guest: str) -> list:
     """Removes a person from the guest list and replaces them with a new guest. """
+    guest_list.remove(guest)
+    new_guest = input("Who would you like to add to the guest list? ").strip().title()
+    guest_list.append(new_guest)
+    print(f"Your new guest list includes: {guest_list}.")
+    return guest_list
+
 
 
 guest_list = []
 while True:
-    option = input("Would you like to: Add people to the list? [1], Replace someone in the list? [2], Print out a list of the invitations that will be sent out? [3], Exit the program? [4]")
+    # Different option values (1-4) call different functions.
+    option = input("Would you like to: Add people to the list? [1], Replace someone in the list? [2], Print out a list of the invitations that will be sent out? [3], Exit the program? [4]").strip()
     
-    if int(option) == 1:
-        # Function "add" called. Adds a chosen number of people to "guest_list"
+    if option == "1": 
         add()
-
-    elif int(option) == 2:
-        # Function remove_and_replace (rar) called. 
+    
+    elif option == "2":
+        remove = 1
+        while remove == 1:
+            rem_guest = input("Who is the person you would like to remove from the guest list? ").strip().title()
+            
+            if rem_guest in guest_list:
+                rar(rem_guest)
+                remove = 0
+            else:
+                while True:
+                    choice = input("Whoops! It seems this guest isn't in the list. Would you like again? [1], or go back to the menu? [2]").strip()
+                    if choice == "1":
+                        pass
+                    elif choice == "2":
+                        remove = 0
+                        break
+                    else:
+                        print("Please choose 1 or 2. ")
 
     elif int(option) == 3:
         # Print out invitations for each dinner guest. 
 
     elif int(option == 4):
         # Exit the program.
+        exit()
+    
+    else: 
+        # Improper input fallback.
+        print("Please pick a number from 1-4. ")
 
